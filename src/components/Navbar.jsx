048801eff9aa0b1574cab6a458892c5a06b9adb7
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2025-02-03 21:42:38 +0000

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { navLinks } from "../constants";
+
+//import { logo } from "../assets";
+
+import '../stylesheets/navbarStyle.css'
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link
+          to="/"
+          className="navbar-logo"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          {/*<img src={logo} alt="logo" className="logo" />*/}
+          <p className="logo-text">
+            TharusanV &nbsp;
+            <span className="desktop-only"> | Portfolio</span>
+          </p>
+        </Link>
+
+        <ul className="nav-links">
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className="nav-item"
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
