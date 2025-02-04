a0f559ab5ba44a90fb7462b6ebefbddf8e6594a1
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2025-02-04 19:09:21 +0000

    Starting hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../stylesheets/heroStyle.css'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero'>
+      <div className='hero-header-container'>
+        <div>
+          <h1 className='hero-header-text'>
+            {"<h1> "} 
+            <span className='hero-header-span'>Hi, I'm Tharusan</span>
+            {" </h1>"}
+          </h1>
+          {/*
+          <p className="hero-header-description">
+            {"<p></p>"} 
+          </p>
+          */}
+        </div>
+      </div>
+
+
+      
+    </section>
   )
 }
 
