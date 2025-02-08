51a6110146e76dc2e2f1fc006639b7aa645e3785
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2025-02-08 18:51:36 +0000

    Project section decent

--- src/assets/index.js
+++ src/assets/index.js
@@ -12,9 +12,15 @@ import aboutIcon from './aboutIcon.png'
 import educationIcon from './education.png'
 import projectIcon from './project.png'
 import programmingIcon from './programming.png'
+
 import fyp from './fyp-project.gif'
 import game from './flfGame.png'
 import rss from './rss.png'
+import chess from './chess.png'
+import poke from './poke.png'
+import media from './media-review.png'
+import eCommerce from './e-commerce.png'
+import fighting from './fight.png'
 
 export {
   menu,
@@ -34,4 +40,9 @@ export {
   fyp,
   game,
   rss,
+  chess,
+  poke,
+  media,
+  eCommerce,
+  fighting,
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 import '../stylesheets/projectStyle.css'
 
-import { fyp, rss, game, } from '../assets';
+import { fyp, rss, game, chess, poke, fighting, eCommerce, media} from '../assets';
 
 const Projects = () => {
   
@@ -27,6 +27,31 @@ const Projects = () => {
         <div className='project-container' onClick={() => handleClick("https://github.com/TharusanV/RSS-Reader")}>
           <h1 className='title'>Fast Lane Feast</h1>
           <img style={{width: "212px", height: "400px"}} src={game} alt="fyp gif" />
+        </div>
+
+        <div className='project-container' onClick={() => handleClick("https://github.com/TharusanV/Chess")}>
+          <h1 className='title'>Chess</h1>
+          <img style={{width: "212px", height: "400px"}} src={chess} alt="fyp gif" />
+        </div>
+
+        <div className='project-container' onClick={() => handleClick("https://github.com/TharusanV/Pokemon")}>
+          <h1 className='title'>Pokemon</h1>
+          <img style={{width: "212px", height: "400px"}} src={poke} alt="fyp gif" />
+        </div>
+
+        <div className='project-container' onClick={() => handleClick("https://github.com/TharusanV/media-review-app")}>
+          <h1 className='title'>Media Review</h1>
+          <img style={{width: "212px", height: "400px"}} src={media} alt="fyp gif" />
+        </div>
+
+        <div className='project-container' onClick={() => handleClick("https://github.com/TharusanV/e-commerce-site")}>
+          <h1 className='title'>E-Commerce</h1>
+          <img style={{width: "212px", height: "400px"}} src={eCommerce} alt="fyp gif" />
+        </div>
+
+        <div className='project-container' onClick={() => handleClick("https://github.com/TharusanV/Fighting-Game")}>
+          <h1 className='title'>Fighting Game</h1>
+          <img style={{width: "212px", height: "400px"}} src={fighting} alt="fyp gif" />
         </div>
 
       </div>
