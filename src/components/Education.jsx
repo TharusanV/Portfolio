86ec2805f9033427f0b3f5a57c139de01a1e5ed5
TharusanV <tharusan.vijayakumar@gmail.com>
2025-02-10 16:15:48 +0000

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,10 +9,24 @@ const Education = () => {
       <div className='education-container'>
         <h1>BSc (Hons) Computer Science</h1>
         <p>First Class Degree<br/>
-          2021-2024<br/> <br /> 
-
-          
+          2021-2024<br/>   
         </p>
+
+        <br /> 
+
+        <h4>Y3 Modules:</h4>
+        <span>
+          Computer Science Final Year Project – A<br/>
+          Software Engineering – A+<br/>
+          Advanced Topics in Computer Science – A+<br/>
+          Human Computer Interaction (HCI) – A*<br/>
+          Digital Media and Games – A-<br/>
+        </span>
+
+        <br /> 
+
+
+
       </div>
 
     </section>
